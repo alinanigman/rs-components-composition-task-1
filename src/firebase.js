e299b30a76da6a1854d7bf6c798542cb3b2fe6d4
alinakassym <alinakassym@gmail.com>
2025-07-30 17:40:31 +0500

    chore: firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAU4ccurypTgOlrXyeljI8c86lGD9uDllA",
+  authDomain: "tic-tac-toe-ace0b.firebaseapp.com",
+  projectId: "tic-tac-toe-ace0b",
+  storageBucket: "tic-tac-toe-ace0b.firebasestorage.app",
+  messagingSenderId: "676273348461",
+  appId: "1:676273348461:web:b79b078cbd09debcf18464",
+};
+
+const app = initializeApp(firebaseConfig);
