78118a9017a9246f95a0de357bb209b4b36c68f7
alinakassym <alinakassym@gmail.com>
2025-07-30 17:52:56 +0500

    fix: game result display when isDraw

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -2,10 +2,10 @@ import InformationLayout from "./InformationLayout";
 
 const Information = ({ currentPlayer, isGameEnded, isDraw }) => {
   const title = `Current Player: ${currentPlayer}`;
-  const status = isGameEnded
-    ? `Player ${currentPlayer} WINS!`
-    : isDraw
-      ? "It's a Draw!"
+  const status = isDraw
+    ? "It's a Draw!"
+    : isGameEnded
+      ? `Player ${currentPlayer} WINS!`
       : "Game in Progress";
 
   return <InformationLayout title={title} status={status} />;
