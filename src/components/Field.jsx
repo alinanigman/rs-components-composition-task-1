98a11a9e9a6eb3839a7f4b0c3b5d4bc777451037
alinakassym <alinakassym@gmail.com>
2025-08-05 18:33:48 +0500

    fix(Field): rm field prop

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,7 +1,7 @@
 import { useStoreSelector } from "@/hooks/useStoreSelector";
 import FieldLayout from "./FieldLayout";
 
-const Field = ({ field, onCellClick }) => {
+const Field = ({ onCellClick }) => {
   const field = useStoreSelector((s) => s.field);
   return <FieldLayout field={field} onCellClick={onCellClick} />;
 };
